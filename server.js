32d25658b447e80d198282274b05f5aea88a5089
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2022-02-13 23:21:48 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+//require dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+//Initalize express 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Setup data passing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//require routes
+require('./routes/routes')(app);
+
+//Setup listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
